c85358e0d5894b84a92de1d01251814e9f575013
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-08-10 23:59:17 +0100

    Added helper function to test (since no tests will be added to due to time constrains).

--- app/lib/deck.rb
+++ app/lib/deck.rb
@@ -1,3 +1,5 @@
+require './card'
+
 class Deck
   POSSIBLE_RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A']
   POSSIBLE_SUITS = %w(C D H S)
--- app/lib/hand.rb
+++ app/lib/hand.rb
@@ -3,9 +3,44 @@ require './deck'
 
 class Hand
   include ActiveModel::Model
+
   attr_accessor :cards, :ranks, :suits
+
+  def initialize
+    super
+    test_helper
+  end
 
   def consecutive?
     self.ranks.each_cons(2).all? { |c1, c2| c1 == (c2 - 1)}
+  end
+
+  # since time does not allow for writing tests
+  def test_helper
+    self.cards = []
+    self.ranks = []
+    self.suits = []
+
+    card = Card.new(:suit => 'C', :rank => 2)
+      self.cards << card
+      self.ranks << card.rank
+      self.suits << card.suit
+    card = Card.new(:suit => 'C', :rank => 4)
+      self.cards << card
+      self.ranks << card.rank
+      self.suits << card.suit
+    card = Card.new(:suit => 'C', :rank => 3)
+      self.cards << card
+      self.ranks << card.rank
+      self.suits << card.suit
+    card =  Card.new(:suit => 'C', :rank => 6)
+      self.cards << card
+      self.ranks << card.rank
+      self.suits << card.suit
+    card =  Card.new(:suit => 'H', :rank => 5)
+      self.cards << card
+      self.ranks << card.rank
+      self.suits << card.suit
+    self.ranks = self.ranks.sort
   end
 end
