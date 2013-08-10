cfd50b55cbdbb99d537e862d1f983899a657f967
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-08-10 23:38:48 +0100

    Added basic card class with validations.

---
+++ app/lib/card.rb
@@ -0,0 +1,14 @@
+require 'active_model'
+
+class Card
+  include ActiveModel::Model
+
+  attr_accessor :rank, :suit
+  validates_presence_of :rank, :suit
+
+  def initialize(opts)
+    super
+    self.rank = opts[:rank]
+    self.suit = opts[:suit]
+  end
+end
