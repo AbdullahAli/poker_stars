0ca81d38a73d1dbc2541f9fa82c455a9e4088f7d
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-08-11 00:36:41 +0100

    Added extra validations for the suit and rank of the card class.

--- app/lib/card.rb
+++ app/lib/card.rb
@@ -1,10 +1,13 @@
 require 'active_model'
+require './deck'
 
 class Card
   include ActiveModel::Model
 
   attr_accessor :rank, :suit
   validates_presence_of :rank, :suit
+  validates_inclusion_of :rank, :in => Proc.new{ Deck::POSSIBLE_RANKS }
+  validates_inclusion_of :suit, :in => Proc.new{ Deck::POSSIBLE_SUITS }
 
   def initialize(opts)
     super
