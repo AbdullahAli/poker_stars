fff7b7756d66f9e3008b2a2346a497930dab736b
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-08-11 00:13:37 +0100

    Made helper methods private and grouped them together.

--- app/lib/hand.rb
+++ app/lib/hand.rb
@@ -3,24 +3,11 @@ require './deck'
 
 class Hand
   include ActiveModel::Model
-
   attr_accessor :cards, :ranks, :suits
 
   def initialize
     super
     test_helper
-  end
-
-  def consecutive?
-    self.ranks.each_cons(2).all? { |c1, c2| c1 == (c2 - 1)}
-  end
-
-  def all_same_suit?
-    suits.uniq.size == 1
-  end
-
-  def get_highest_card
-    self.ranks.last
   end
 
   def four_of_a_kind?
@@ -34,10 +21,6 @@ class Hand
     response
   end
 
-  def get_rank_appearances
-    ranks.inject(Hash.new(0)) { |t, e| t[e] += 1; t }
-  end
-
   def straight_flush?
     response = [false]
     is_straight_flush = (consecutive? && all_same_suit?)
@@ -47,6 +30,24 @@ class Hand
     end
 
     response
+  end
+
+  private
+
+  def consecutive?
+    self.ranks.each_cons(2).all? { |c1, c2| c1 == (c2 - 1)}
+  end
+
+  def get_rank_appearances
+    ranks.inject(Hash.new(0)) { |t, e| t[e] += 1; t }
+  end
+
+  def all_same_suit?
+    suits.uniq.size == 1
+  end
+
+  def get_highest_card
+    self.ranks.last
   end
 
   # since time does not allow for writing tests
