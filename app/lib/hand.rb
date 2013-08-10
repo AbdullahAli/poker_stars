390b28b78da41d07729fd22a55f2bae67dbc1eba
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2013-08-11 00:27:43 +0100

    Added full house functionality.

--- app/lib/hand.rb
+++ app/lib/hand.rb
@@ -10,6 +10,20 @@ class Hand
     test_helper
   end
 
+  def full_house?
+    response = [false]
+    rank_appearances = get_rank_appearances
+    is_3_with_same_value, highest_number = has_recurrences_of?(3)
+    rank_appearances.delete(highest_number)
+
+    if is_3_with_same_value
+      if rank_appearances.values.first == 2
+        response = [true, highest_number]
+      end
+    end
+    response
+  end
+
   def four_of_a_kind?
     response = [false]
     get_rank_appearances.any? do |k,v|
@@ -34,11 +48,34 @@ class Hand
 
   private
 
+  def has_recurrences_of?(required_count)
+    response = [false]
+    matched = false
+    highest_number = 0
+
+    get_rank_appearances(ranks).each do |k,v|
+      if v >= required_count
+        matched = true
+
+        if k > highest_number
+          highest_number = k
+        end
+      end
+    end
+
+    if matched
+      response = [true, highest_number]
+    end
+
+    response
+  end
+
   def consecutive?
     self.ranks.each_cons(2).all? { |c1, c2| c1 == (c2 - 1)}
   end
 
-  def get_rank_appearances
+  def get_rank_appearances(ranks = {})
+    ranks = ranks.empty? ? self.ranks : ranks
     ranks.inject(Hash.new(0)) { |t, e| t[e] += 1; t }
   end
 
@@ -72,7 +109,7 @@ class Hand
       self.cards << card
       self.ranks << card.rank
       self.suits << card.suit
-    card =  Card.new(:suit => 'H', :rank => 4)
+    card =  Card.new(:suit => 'H', :rank => 2)
       self.cards << card
       self.ranks << card.rank
       self.suits << card.suit
